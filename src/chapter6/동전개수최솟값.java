0a8266bf04866429e9fc0b7bca5d3e14e250e1da
yujin-Kim-98 <yujin123.kim@gmail.com>
2025-02-01 19:09:46 +0900

    그리디 알고리즘

---
+++ src/chapter6/동전개수최솟값.java
@@ -0,0 +1,33 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class 동전개수최솟값 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (A[i] <= K) {
+                count += (K / A[i]);
+                K = K % A[i];
+            }
+        }
+
+        System.out.println(count);
+
+    }
+
+}
---
+++ src/chapter6/잃어버린괄호.java
@@ -0,0 +1,41 @@
+package chapter6;
+
+import java.util.Scanner;
+
+public class 잃어버린괄호 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String example = sc.nextLine();
+        String[] str = example.split("-");
+
+        int answer = 0;
+
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+
+            if (i == 0) {
+                answer += temp;
+            } else {
+                answer -= temp;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    static int mySum(String a) {
+        int sum = 0;
+        String temp[] = a.split("[+]");
+
+        for (int i = 0; i < temp.length; i++) {
+            sum += Integer.parseInt(temp[i]);
+        }
+
+        return sum;
+    }
+
+}
